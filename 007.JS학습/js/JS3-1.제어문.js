b150f3465b605e14ef7b65ee3c6255aa49f7e99b
Tom B. Parker <tombap8@hotmail.com>
2025-09-09 15:13:02 +0900

    ss

--- 007.JS학습/js/JS3-1.제어문.js
+++ 007.JS학습/js/JS3-1.제어문.js
@@ -24,6 +24,10 @@ function 점수보여줘() {
 
     // 2-2. 출력박스
     var 출력박스 = document.querySelector('.jexp');
+
+    // 2-3. 칭찬스티커 박스
+    var 칭찬스티커 = document.querySelector('.jshow');
+
     // querySelector() 메서드는 선택자(클래스,아이디,태그) 모두 가능함! 하나만 선택하여 가져온다!
     // querySelectorAll() 메서드는 선택자 모두 가능하며, 여러개를 선택하여 가져온다! -> 순번필요! -> item(순번) 사용!
 
@@ -31,6 +35,86 @@ function 점수보여줘() {
         "내점수:", 내점수.value, 
         '출력박스:', 출력박스);
 
+    // 3. 점수에 따른 if문 처리하기 /////
+    if(내점수.value >= 90) { // 90점 이상
+        출력박스.innerHTML = "매우잘함!";
+        출력박스.style.color = "blue";
+        칭찬스티커.style
+        .backgroundPosition = "50% 50%";
+    }
+    else if(내점수.value >= 80) { // 80점 이상
+        출력박스.innerHTML = "잘함!";
+        출력박스.style.color = "green";
+        칭찬스티커.style
+        .backgroundPosition = "100% 0%";
+    }
+    else if(내점수.value >= 70) { // 70점 이상
+        출력박스.innerHTML = "보통!";
+        출력박스.style.color = "purple";
+        칭찬스티커.style
+        .backgroundPosition = "0% 100%";
+    }
+    else if(내점수.value >= 60) { // 60점 이상
+        출력박스.innerHTML = "노력요함!";
+        출력박스.style.color = "orange";
+        칭찬스티커.style
+        .backgroundPosition = "100% 100%";
+    }
+    else { // 60점 이하
+        출력박스.innerHTML = "재시험!";
+        출력박스.style.color = "red";
+        칭찬스티커.style
+        .backgroundPosition = "0% 100%";
+    }
 
 
 } // 점수보여줘 함수 ////
+
+
+/*************************************** 
+     [ if문 ]
+    - 조건문을 검사하여 true이면
+    중괄호안의 코드가 실행되는 제어문
+
+    - 조건문이란 
+    결과가 true / false 로 나오는 구문
+    (조건문: 비교연산자, 논리연산자)
+
+    ((구문))
+    ____________________________
+
+    if(조건문){
+        실행코드
+    }
+    else if(조건문){
+        실행코드
+    }
+    ...
+    else{
+        실행코드
+    }
+    ______________________________
+
+    ((구문해석))
+    1. if문은 단독으로 따로 사용가능
+
+    2. else if문은 if문 뒤에 또는 else if문 뒤에
+    만 사용가능함!(else if로 시작못함!)
+    -> 하나의 조건에 걸리면 다른 조건문엔 안들어감!
+    -> 단독으로 else if문 사용불가!
+
+    3. else문은 모든 조건에 해당없을때
+    무조건 실행할 코드가 있을 경우 사용한다!
+    -> if문 가장 끝에 사용한다!
+    -> 단독으로 else문만 사용불가!
+
+    ※ 참고 : if문의 실행문이 하나이면 중괄호생략가능
+    예) 
+        if(aa>10){
+            my = "ㅎㅎㅎ";
+        }
+
+        -> 위와 같음
+
+        if(aa>10) my = "ㅎㅎㅎ";
+    ***************************************/
