283d73e4149c0cbe85cbddbd1c8f0258e6259bc4
Tom B. Parker <tombap8@hotmail.com>
2025-10-02 13:03:47 +0900

    ss

--- 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main.js
+++ 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main.js
@@ -32,7 +32,7 @@ setInterval(setClass, 10000);
 setClass();
 
 /************************************************ 
-    최신 동영상 파트에 스와이퍼 적용하기
+    2. 최신 동영상 파트에 스와이퍼 적용하기
 ************************************************/
 const videoSwiper = new Swiper(".clip-box", {
   // 자동플레이설정
@@ -110,3 +110,19 @@ videoSwiper.on("slideChange", () => {
     btnPrev.style.display = "block";
   } /// else ////
 }); ///// slideChange //////
+
+
+
+/************************************************ 
+    3. 햄버거 버튼 클릭시 상단영역에 클래스넣기
+************************************************/
+// (1) 이벤트 대상 : .btn-ham
+const $btnHam = $(".btn-ham");
+// (2) 변경 대상 : #top-area
+const $topArea = $("#top-area");
+
+// (3) 이벤트 대상 클릭시 
+// 변경대상에 클래스 토글로 on넣기
+$btnHam.on("click", () => {
+  $topArea.toggleClass("on");
+}); /// click ///
