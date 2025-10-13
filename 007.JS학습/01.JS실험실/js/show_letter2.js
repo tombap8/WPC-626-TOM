d8cf8037fc3e4047f7d4fe72bdfa34896348129b
Tom B. Parker <tombap8@hotmail.com>
2025-10-13 14:15:18 +0900

    Update show_letter2.js

--- 007.JS학습/01.JS실험실/js/show_letter2.js
+++ 007.JS학습/01.JS실험실/js/show_letter2.js
@@ -18,13 +18,43 @@ const myText =
 // 4. 글자 데이터 변환하기
 // - 기준: 띄어쓰기 공백
 // - 잘라서 배열만드는 메서드는? split(자를문자기준)
+const newText = myText.split(" ");
+console.log("잘라서배열:", newText);
 
+// forEach() 메서드를 사용하는 방법 //////
+// 배열값 만큼 반복시킨다!
+// 이때 변수에 span태그로 싼 값을 담는다!
+// let hCode = "";
+// newText.forEach(val=>{
+//     hCode += `<span>${val}</span>`;
+// });
+// // 대상박스에 코드넣기
+// stage.innerHTML = hCode;
+
+// 위와 같이 forEach를 사용할때는 변수도 써야하고
+// 따로 html넣기도 해야하는 번거로움이 있다!
+// 이런 이유로 새로운 메서드가 생겼으니 바로 -> map()
+// map((값,순번,객체)=>{return 새로운값})
+// -> 만약 return 키워드를 생력하려면?
+// map((값,순번,객체)=>새로운값)
 
 // 5. 글자데이터 변환하기
 // - span태그로 싸서 대상박스에 넣기
 // 대상: stage
-
-
+stage.innerHTML = newText
+  .map(
+    (v, i) =>
+      `<span
+            style="transition-delay: ${i * 0.1}s"
+        >${v}</span>`
+  )
+  .join("");
+// map의 결과는 배열이기때문에 그 값을 문자화하여
+// 콤마를 없애면 된다! 이때 쓰는 배열 메서드는?
+// 배열.join('') -> 구분자를 빈것으로 하면 깨끗한
+// 태그만 남는다!!!
+// 객체는 맵쪼잉!
+// 배열객체.map().join('')
 
 // 사용할 수 있는 방법은?
 // 1) for of문
@@ -36,10 +66,44 @@ const myText =
 // -> 변경된 데이터를 새로운 배열로 만들 수 있음
 // -> 원본배열은 보존된다!
 
+const aa = ["헐", "할", "핼"];
+console.log("aa배열:", aa);
+// 기존배열값에 하트를 넣는다
+console.log(
+  "하트넣은 aa배열:",
+  aa.map((v) => "♥" + v)
+);
+console.log(
+  "하트넣은 aa배열을 문자열로 출력:",
+  aa.map((v) => "♥" + v).join("ㅋㅋ")
+);
+console.log(
+  "div태그로 aa배열을 싸고 문자열로 출력:",
+  aa.map((v) => `<div>${v}</div>`).join("")
+);
+
+console.log("다시 aa배열:", aa);
+// 배열값을 특정 문자열로 결합하여 출력하기
+// 배열.join('문자열') -> 전체 배열이 문자열로 출력
+
+// [ 배열값을 map으로 변형후 출력하기! ]
+// 배열.map().join('')
+// 배열값 변형 맵죠잉~!
 
 // 등장액션 기준값설정(윈도우화면 절반크기)
 const CRITERIA = window.innerHeight / 2;
 console.log("기준값:", CRITERIA);
 
 // 6. 스크롤 시 글자박스에 클래스 넣기
+myFn.addEvt(window, "scroll", () => {
+  // (1) 대상요소 위치값 : getBoundingClientRect()
+  // 대상: stage
+  let pos = myFn.getBCR(stage);
 
+  console.log("스크롤~~~", pos);
+
+  // (2) 바운딩값이 기준값보다 작을때
+  // 대상요소에 클래스 "on"넣기
+  if (pos < CRITERIA) stage.classList.add("on");
+  // else stage.classList.remove('on');
+}); /////// addEvt /////////////
