601d1e2f4af1a3736fd3b6722d3e3cc39a6dee24
Tom B. Parker <tombap8@hotmail.com>
2025-10-15 11:57:45 +0900

    Update main.js

--- 009.jQuery학습/003.응용구현/01.Ossomae_PJ/js/main.js
+++ 009.jQuery학습/003.응용구현/01.Ossomae_PJ/js/main.js
@@ -19,30 +19,35 @@
 // 변경대상 변수할당 : .gbx>div
 const $target = $('.gbx');
 
-// 광클금지상태변수
-let stopClick = false;
-// 값이 true일때 클릭허용/ false면 불허용
 
-// 광클금지해제시간 상수셋팅
-const TIME_GAP = 400;
 
 // 1. 오른쪽 버튼 클릭시
 $('.rb').click(()=>{
     // 광클금지함수호출후 리턴셋팅!
     if(blockClick()) return;
-
+    // 맨앞div 맨뒤로 이동
     $target.append($target.find('div').first());
-});
+}); /// click //////////////////
 
 // 2. 왼쪽 버튼 클릭시
 $('.lb').click(()=>{
     // 광클금지함수호출후 리턴셋팅!
     if(blockClick()) return;
-    
+    // 맨뒤div 맨앞으로 이동
     $target.prepend($target.find('div').last());
-});
+}); /// click //////////////////
 
-//////// 광클금지함수 ///////
+/******************************** 
+////////// 광클금지함수 //////////
+********************************/
+// [1] 광클금지상태변수 ///////////
+let stopClick = false;
+// 값이 true일때 클릭허용/ false면 불허용
+
+// [2] 광클금지해제시간 상수셋팅 //////
+const TIME_GAP = 400;
+
+// [3] 광클금지함수 //////////////////
 function blockClick(){
     // 1. 광클이면 true 를 리턴함!
     if(stopClick) return true;
@@ -57,6 +62,30 @@ function blockClick(){
     return false;
 } ////// blockClick 함수 ///////
 
+
+/*************************************** 
+/////////// 자동넘김 셋팅하기 ///////////
+***************************************/
+// [1] 인터발, 타임아웃 저장용 변수
+let autoI, autoT;
+// -> 이변수는 clear시 사용함!
+
+// [2] 인터발, 타임아웃 시간 상수셋팅
+const IV_TIME = 2000;
+const TO_TIME = 5000;
+
+// 자동호출함수 최초호출 ////
+slideAuto();
+
+// [3] 자동호출함수 ////////////////
+function slideAuto(){
+    // 지우기위해 전역변수 autoI에 할당함
+    autoI = setInterval(() => {
+        // 맨앞div 맨뒤로 이동
+        $target.append($target.find('div').first());
+    }, IV_TIME);
+
+} ////// slideAuto 함수 //////
 
 
 
