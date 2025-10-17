29af1fcfcbddfe7562f4babbdb5c06567a5048c2
Tom B. Parker <tombap8@hotmail.com>
2025-10-17 13:17:22 +0900

    ss

--- 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/common.js
+++ 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/common.js
@@ -1,9 +1,8 @@
 // 도깨비 PJ 공통 JS - common.js /////////
 
 // 배너 슬라이드 함수 불러오기
-import bannerFn from './main1_jquery.js';
+import bannerFn from "./main1_jquery.js";
 // default로 내보냈으므로 아무이름으로 받아도됨!
-
 
 // 같은 이름의 변수의 충돌을 막기위해 지역변수화를 해준다!
 // 방법은 (()=>{나의코드})() 익명함수를 바로 실행하는 지역코드로 감싸준다!
@@ -13,24 +12,26 @@ import bannerFn from './main1_jquery.js';
 /// 지역화 코드 시작 //////////////
 (() => {
   // 1. 상단, 하단 공통 모듈 html넣기
-  // (1) 대상 : #top-area, #bottom-area, .banner-part
+  // (1) 대상 : 상단영역 #top-area
   const $topArea = $("#top-area");
+  // 하단영역 #bottom-area
   const $bottomArea = $("#bottom-area");
+  // 배너영역 .banner-part
   const $bannerPart = $(".banner-part");
+  // 드라마 파트메뉴 #spart-menu
   const $spartMenu = $("#spart-menu");
-
 
   // (2) 대상에 load() 메서드로 html넣기
   // load(파일경로, 로딩후실행함수)
   // (2-1) 상단부 html넣기
-  $topArea.load("./inc/header.html",headerFn);
+  $topArea.load("./inc/header.html", headerFn);
   // -> 상단부 html파일이 모두 로딩된후 headerFn함수가 실행됨!
 
   // (2-2) 하단부 html넣기
   $bottomArea.load("./inc/footer.html");
 
   // (2-3) 배너부 html넣기 : 로딩후 배너함수호출!
-  $bannerPart.load("./inc/banner.html",bannerFn);
+  $bannerPart.load("./inc/banner.html", bannerFn);
 
   // (2-4) 드라마 파트메뉴 html넣기
   $spartMenu.load("./inc/part_menu.html");
@@ -69,4 +70,65 @@ function headerFn() {
 
   // 1-4. 처음에 회전하도록 클래스 셋팅함수 호출하기
   setClass();
+
+  /************************************************ 
+    2. 햄버거 버튼 클릭시 상단영역에 클래스넣기
+************************************************/
+  // (1) 이벤트 대상 : .btn-ham
+  const $btnHam = $(".btn-ham");
+  // (2) 변경 대상 : #top-area
+  const $topArea = $("#top-area");
+
+  // (3) 이벤트 대상 클릭시
+  // 변경대상에 클래스 토글로 on넣기
+  $btnHam.on("click", () => {
+    $topArea.toggleClass("on");
+  }); /// click ///
+
+  /************************************************ 
+    3. 상위메뉴 li 클릭시 서브메뉴에 클래스 넣기
+    ->  클래스 on 을 넣어서 서브메뉴가 등장함!
+************************************************/
+  // (1) 이벤트 대상 : .gnb-menu > ul > li
+  const $gnbList = $(".gnb-menu > ul > li").has(".sub-menu");
+  // has(선택요소) 메서드 -> 자식으로 선택요소가 있는 요소를 선택
+  // -> .sub-menu가 있는 li를 선택
+  // console.log($gnbList);
+
+  // 하위 메뉴 보이기 숨기기 할때 메뉴박스의 z-index:1 처리위해 대상선정
+  const $menuBox = $(".menu-box");
+
+  // (2) 이벤트 함수 구현하기 ////
+  $gnbList.click(function () {
+    // this 키워드로 클릭된 li 자신을 선택하여
+    // 하위의 .sub-menu에 클래스 on을 넣기
+    $(this).find(".sub-menu").addClass("on");
+    // addClass() 메서드 -> 선택된 요소에 클래스를 넣기
+
+    // 메뉴박스 z-index:1처리
+    $menuBox.css("z-index", "1");
+  }); /// click ///
+
+  // (3) 리스트 하위의 a요소 클릭시 페이지이동 특성막기!
+  $gnbList.find("a").click((e) => e.preventDefault());
+
+  // (4) 이전 이동버튼 클릭시 부모 .sub-menu의 클래스 on 제거하기
+  $(".btn-up-menu").click(function (e) {
+    // e - 이벤트 전달변수
+    // console.log('이전 이동버튼 클릭');
+    // 클릭된 버튼의 부모들중 .sub-menu에 클래스 on 제거
+    $(this).parents(".sub-menu").removeClass("on");
+    // parents(특정부모요소) 메서드 -> 부모요소들 중 특정부모요소를 선택
+    // 비교) parent() 메서드 -> 바로 상위 직계부모요소 선택
+    // removeClass() 메서드 -> 선택된 요소에 클래스를 제거
+
+    // 주의! 이전 이동버튼은 부모 li의 자식이므로 클릭시
+    // 이벤트 버블링이 일어나서 부모 li가 다시 클릭된다!
+    // 따라서 on을 제거후 다시 on이 추가되어 아무일도 없는 것처럼 보인다!
+    // 여기서 이벤트 버블링 막기가 필요하다!
+    e.stopPropagation();
+
+    // 메뉴박스 z-index:0처리
+    $menuBox.css("z-index", "0");
+  }); /// click ///
 } ////////////// headerFn ///////////////
--- 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main1_jquery.js
+++ 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/main1_jquery.js
@@ -1,56 +1,184 @@
-// 가로방향 배너 슬라이드
-// 제이쿼리 버전 - main1_jquery.js
+// 쇼핑몰 배너 JS -> 제이쿼리버전!
+// 01.가로방향 배너 슬라이드 - 무한이동 //
 
-// 내보내기 처리하기 //////
-// default로 내보내면 받는 곳에서 아무이름이나
-// 사용해서 받을 수 있다!!!
 export default function main1_jquery() {
-  // 1. 대상
-  // 1-1. 이벤트 대상 : 이동버튼 2개 - .ab1, .ab2
-  // 1-2. 변경 대상 : 슬라이드 박스 - .banner-slide
-  const $slide = $(".banner-slide");
-  // 보통 제이쿼리 선택할당은 변수명 앞에 $로 시작
+// 1. 대상선정 //////////////////
+// (1) 전체 슬라이드 박스 : .slide-box
+const $slideBox = $(".banner-box");
+// 전체 박스 하위의 요소로 상대적으로 잡아준다!
+// 이유는 다른 슬라이드 박스를 카피하여 만든경우
+// 동일한 기능이 될 수 있게 해준다!
 
-  // 2. 구현하기
-  // 2-1. 오른쪽 이동버튼 클릭시 기능구현
+// (2) 이벤트대상: .abtn
+const $abtn = $slideBox.find(".abtn");
 
-  // 슬라이드 순번 전역변수
-  let seq = 0;
+// (3) 변경대상: .slide
+const $slide = $slideBox.find(".banner-slide");
 
-  // 오른쪽 버튼은 재사용되므로 변수에 담자!
-  const $rbtn = $(".ab2");
+// (4) 최초 슬라이드 li 수집하기
+const $firstSlide = $slide.find("li");
 
-  // 클릭이벤트 설정하기 ///
-  $rbtn.click(() => {
-    // 슬라이드 순번 전역변수 1증가
-    seq++;
-    // 2가 한계이므로 처리
-    if (seq > 2) seq = 0;
+// 최초슬라이드 li에 data-seq 속성 만들고 순번넣기
+// 왜 넣는가? 슬라이드 li순서가 계속 변경되므로
+// 블릿 인디케이터의 표시 순서를 잡기 위해 넣어준다!
 
-    // 슬라이드의 translate값을 변경함!
-    $slide.css({
-      translate: -100 * seq + "%",
-      transition: ".5s",
-    }); /// css ///
-  }); /// click ///
+// [ 제이쿼리 each() 메서드 ]
+// 선택요소 개수 만큼 자동으로 반복해준다!
+// -> each((순번,요소)=>{})
+$firstSlide.each((idx, el) => {
+  // 속성셋팅은 attr(속성명,값)
+  $(el).attr("data-seq", idx);
+}); ////// each 메서드 /////////////
 
-  // 클릭이벤트 설정하기 ///
-  $(".ab1").click(() => {
-    // 슬라이드 순번 전역변수 1감소
-    seq--;
-    // 0이 한계이므로 처리
-    if (seq < 0) seq = 2;
+// 슬라이드 개수 변수할당!
+// 보통 변경없이 사용하는 변수는 상수라고 하고
+// 상수는 보통 대문자로 쓰고 스네이크 케이스 사용함!
+const SLIDE_CNT = $firstSlide.length;
+console.log("슬라이드개수:", SLIDE_CNT);
 
-    // 슬라이드의 translate값을 변경함!
-    $slide.css({
-      translate: -100 * seq + "%",
-      transition: ".5s",
-    }); /// css ///
-  }); /// click ///
+// (4) 인디케이터 블릿대상
+// const $indic = $slideBox.find(".indic li");
 
-  // 일정 간격으로 오른쪽 버튼 클릭설정하기 ///
-  setInterval(() => {
-    // 오른쪽 버튼 클릭 강제발생
-    $rbtn.click();
-  }, 3000); // 3초간격
-} ////////// main1_jquery /////////////
+// console.log("대상:",$slideBox,$abtn,$slide,$indic);
+
+// 2. 이벤트 설정하기 ////////////////////
+// 버튼 클릭시 슬라이드 이동하기
+
+// 오른쪽 버튼 클릭시
+// 원리: translate X축 이동값을 -100%로 변경
+$abtn.click(function () {
+  // 광클금지함수 호출 셋팅하기
+  if (blockClick()) return;
+
+  // 버튼구분하기 : 오른쪽버튼(.ab2)이면 true
+  let isR = $(this).is(".ab2");
+  console.log("오른쪽?", isR);
+
+  // animate({CSS변경},시간,이징,함수)
+
+  // 1. 오른쪽 버튼일때
+  if (isR) {
+    $slide.animate(
+      { translate: "-100%" }, // CSS변경
+      TIME_GAP, // 시간
+      () => {
+        // 애니후 실행함수 시작
+        $slide // 주인공은? 슬라이드!
+          // (1) 맨앞요소 맨뒤로 이동
+          .append($slide.find("li").first())
+          // (2) 이때 translate값 초기화
+          .css({ translate: "0%" });
+      } /// 애니후 실행함수 끝 ///
+    ); /// animate //////
+  } //// if //////
+
+  // 2. 왼쪽 버튼일때 //////
+  else {
+    $slide // 주인공은? 슬라이드!
+      // (1) 슬라이드가 먼저 맨뒤li를 맨앞으로 이동
+      .prepend($slide.find("li").last())
+      // (2) 이때 translate값 초기화
+      .css({ translate: "-100%" })
+      // (3) 오른쪽 방향으로 슬라이드 이동애니
+      .animate({ translate: "0%" }, TIME_GAP);
+    // -> 주의! 0일 경우에도 단위를 반드시 써야
+    // 애니메이션이 잘 적용된다!
+  } //// else //////
+
+  // 3. 슬라이드 위치표시 블릿
+  // (1) 블릿 대상: .indic li
+  // 해당 블릿은 오른쪽 버튼일때 순번 1, 왼쪽버튼일때 순번 0 슬라이드의 data-seq값을 읽어오면 된다!
+  // let currIdx = $slide
+  //   .find("li")
+  //   .eq(isR ? 1 : 0)
+  //   .attr("data-seq");
+  // console.log("읽은순번:", currIdx);
+
+  // (2) 블릿 li클래스 on넣기(나머지는 빼기)
+  // $indic.eq(currIdx).addClass("on").siblings().removeClass("on");
+}); ////// click 메서드 //////////
+
+/******************************** 
+////////// 광클금지함수 //////////
+********************************/
+// [1] 광클금지상태변수 ///////////
+let stopClick = false;
+// 값이 true일때 클릭허용/ false면 불허용
+
+// [2] 광클금지해제시간 상수셋팅 //////
+const TIME_GAP = 400;
+
+// [3] 광클금지함수 //////////////////
+function blockClick() {
+  // 1. 광클이면 true 를 리턴함!
+  if (stopClick) return true;
+
+  // 2. 클릭가능상태이면 전역변수 셋팅
+  stopClick = true;
+  setTimeout(() => {
+    stopClick = false;
+  }, TIME_GAP);
+
+  // 3. 상태값 리턴 (클릭가능상태 false)
+  return false;
+} ////// blockClick 함수 ///////
+
+/*************************************** 
+/////////// 자동넘김 셋팅하기 ///////////
+***************************************/
+// [1] 인터발, 타임아웃 저장용 변수
+let autoI, autoT;
+// -> 이변수는 clear시 사용함!
+
+// [2] 인터발, 타임아웃 시간 상수셋팅
+const IV_TIME = 2000;
+const TO_TIME = 5000;
+
+// 자동호출함수 최초호출 ////
+slideAuto();
+
+// [3] 자동호출함수 ////////////////
+function slideAuto() {
+  // 지우기위해 전역변수 autoI에 할당함
+  autoI = setInterval(() => {
+    // 1. 오른쪽버튼 클릭시 작동과 동일!
+    $slide.animate(
+      { translate: "-100%" }, // CSS변경
+      TIME_GAP, // 시간
+      () => {
+        // 애니후 실행함수 시작
+        $slide // 주인공은? 슬라이드!
+          // (1) 맨앞요소 맨뒤로 이동
+          .append($slide.find("li").first())
+          // (2) 이때 translate값 초기화
+          .css({ translate: "0%" });
+      } /// 애니후 실행함수 끝 ///
+    ); /// animate //////
+
+    // 2. 슬라이드 위치표시 블릿
+    // (1) 블릿 대상: .indic li
+    // 해당 블릿은 오른쪽 버튼일때 순번 1 슬라이드의 data-seq값을 읽어오면 된다!
+    // let currIdx = $slide.find("li").eq(1).attr("data-seq");
+    // console.log("읽은순번:", currIdx);
+
+    // (2) 블릿 li클래스 on넣기(나머지는 빼기)
+    // $indic.eq(currIdx).addClass("on").siblings().removeClass("on");
+  }, IV_TIME);
+} ////// slideAuto 함수 //////
+
+// 이동버튼 클릭시 지우기함수 호출하기 ////
+$(".abtn").click(clearAuto);
+
+// [4] 지우기 함수 /////////////////
+function clearAuto() {
+  // 1. 인터발 지우기
+  clearInterval(autoI);
+
+  // 2. 타임아웃 지우기 : 실행쓰나미 방지!!!
+  clearTimeout(autoT);
+
+  // 3. 타임아웃 셋팅하기(일정시간후 다시 자동호출)
+  autoT = setTimeout(slideAuto, TO_TIME);
+} ////// clearAuto 함수 //////
+
+} ///////// 전체 함수 끝 ///////////////////
