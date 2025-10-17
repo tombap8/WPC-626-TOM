43c830a4eb25dc19f662d46919fe1778cc14cfe6
Tom B. Parker <tombap8@hotmail.com>
2025-10-17 11:10:31 +0900

    Update auto_scroll.js

--- 007.JS학습/01.JS실험실/js/auto_scroll.js
+++ 007.JS학습/01.JS실험실/js/auto_scroll.js
@@ -21,13 +21,39 @@ let pgNo = 0;
 // 7. 이동단위 -> 윈도우 높이값
 let winH = window.innerHeight;
 
+// 8. 전체 .page인 페이지개수 구하기 - 페이지 증가 한계값으로 사용
+const page = document.querySelectorAll('.page');
+const pageCnt = page.length;
+
+console.log('페이지개수:',pageCnt);
+
 // 8. 휠 이벤트를 window에 적용하여 기본 휠작동 막기
 // -> 그래야 우리가 원하는 자동 스크롤 기능을 구현할 수 있다
 window.addEventListener('wheel',(e)=>{ // e - 이벤트 전달변수
-    // 기본 기능 막기
+    // (1) 기본 기능 막기
     e.preventDefault();
     // -> window / document / body 에서 기본기능막기를 할때
     // passive 모드값을 false로 설정해야 에러가 발생하지 않음
-    console.log('휠~~~~!');
+    
+    // (2) 휠 방향 알아내기
+    // 이벤트객체.wheelDelta
+    let dir = e.wheelDelta;
+    // 아래쪽은 음수, 윗쪽은 양수    
+    
+    // (3) 방향에 따른 페이지번호 증감
+    if(dir < 0){ //  아랫방향 음수
+        pgNo++; // 페이지번호 증가
+        if(pgNo >= pageCnt) pgNo = pageCnt - 1; // 한계값 고정
+    } /// if ///
+    else if(dir > 0){ // 윗방향 양수
+        pgNo--; // 페이지번호 감소
+        if(pgNo < 0) pgNo = 0; // 한계값 고정
+    } /// else if ///
+    
+    console.log('휠~~~~!',pgNo);
+    
+    // (4) 전체 포지션이동
+    window.scrollTo(0,pgNo * winH);
+
 },{passive:false}); //////////// wheel 이벤트 //////////////
 // addEventListener(이벤트명,함수,{passive:false});
