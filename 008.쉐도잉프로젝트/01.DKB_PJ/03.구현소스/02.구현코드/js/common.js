b21b6c3bb9156bdec61675a98689ca6fd8516e22
Tom B. Parker <tombap8@hotmail.com>
2025-10-20 13:18:27 +0900

    Update common.js

--- 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/common.js
+++ 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/common.js
@@ -40,6 +40,27 @@ import bannerFn from "./main1_jquery.js";
 
 /// 2. 상단파트에서 실행할 함수 /////////////
 function headerFn() {
+ /***************************** 
+    JS 로 링크 시스템 만들기
+ *****************************/
+// 대상 : 상단영역 a요소
+const $topLink = $("#top-area a");
+$topLink.click(function(e){ // e - 이벤트 객체
+  // 기본이동 막기
+  e.preventDefault();
+  // 클릭된 a요소의 글자 읽기
+  let aTxt = $(this).text();
+  console.log(aTxt);
+  // 분기하여 이동
+  // 이동은 location.href = '링크주소';
+  switch(aTxt){
+    case "로그인" : location.href = 'login.html'; break;
+    case "회원가입" : location.href = 'member.html'; break;
+  } //// switch문 //////////
+}); /////// click 이벤트 //////////////
+
+
+
   ///////////////////////////////////////////////////
   // 1. 큐브로고박스 일정간격으로 클래스 넣었다 빼기 ///
   //////////////////////////////////////////////////
