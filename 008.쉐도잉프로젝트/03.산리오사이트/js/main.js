7ef7dd5c93a3465497d618ea0ca0e7821117c2fb
Tom B. Parker <tombap8@hotmail.com>
2025-11-10 16:22:58 +0900

    ss

---
+++ 008.쉐도잉프로젝트/03.산리오사이트/js/main.js
@@ -0,0 +1,11 @@
+// 산리오 사이트 메인 JS - main.js
+
+import myFn from "./my_function.js";
+
+// 캐릭터 데이터 가져오기 ////
+import catData from './cat_data.json' with{type:'json'};
+// console.log(catData);
+
+// GNB용 데이터 배열
+const gnbData = ["company","character","place","recruit"];
+
