17a9fa5e1854b824cc113aa0753ead5b38df0aa7
Tom B. Parker <tombap8@hotmail.com>
2025-11-10 12:40:09 +0900

    ss

--- 007.JS학습/js/js9-3.board_test.js
+++ 007.JS학습/js/js9-3.board_test.js
@@ -25,14 +25,35 @@ sbtn.addEventListener('click',()=>{
             cont : "내용"
         }
     */
-   // (2) 기본배열 객체 - 기본값 하나를 넣어 놓는다!
-   const myArr = [
-       {
-           idx : 1,
-           tit : "내가 왕이 될 상인가?",
-           cont : "아니라고하면 넌 어떻게 될까?"
-       }
-   ];
+   // (2) 로컬스토리지에 배열값이 없으면 새로만들고
+   // 있으면 기존값에 데이터를 추가한다!
+   let nowLocal = localStorage.getItem('my-board');
+   console.log('지금로컬쓰있나?',nowLocal);
+
+   // 로컬스토리지에 저장하는 배열
+   let myArr;
+
+   // 만약 로컬쓰가 없으면 null값이 나옴!
+   // 따라서 if문에서 false처리됨!
+   if(nowLocal){ // 값이 있을경우임!
+    // 문자 형식의 로컬스토리지를 배열로 바꾸기
+    // 문자형식의 배열을 다시 원래 배열객체로 변경하는 방법은?
+    // -> JSON.parse(문자형배열)
+    myArr = JSON.parse(nowLocal);
+    console.log(myArr);
+   } //// if /////
+   else {
+    // 로컬스토리지에 배열값이 없음!
+    // 기본배열 객체 새로만들기 - 기본값 하나를 넣어 놓는다!
+    myArr = [
+        {
+            idx : 1,
+            tit : "내가 왕이 될 상인가?",
+            cont : "아니라고하면 넌 어떻게 될까?"
+        }
+    ];
+   } ///// else /////
+
 
    // (3) 방금 입력된 값을 추가!
    myArr.push({
@@ -49,4 +70,58 @@ sbtn.addEventListener('click',()=>{
    // -> JSON.stringify(배열/객체)
    localStorage.setItem('my-board', JSON.stringify(myArr));
 
+   // (5) 화면출력!
+   showBoard(myArr);
+
 }); //////////// click 이벤트함수 //////////
+
+// 출력할 박스는?
+const boardBox = document.querySelector(".board");
+
+// 만약 로컬쓰가 있다면 화면출력하기!
+// 화면출력 함수 ///////////////
+const showBoard = (myFriend) => {
+    // myFriend : 배열 객체 데이터
+    boardBox.innerHTML = `
+    <table>
+        <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>내용</th>
+        </tr>
+        <!-- 데이터에 따른 반복바인딩 -->
+        ${myFriend
+          .map(
+            (v, i) => `
+            <tr>
+                <td>${v.idx}</td>
+                <td>${v.tit}</td>
+                <td>${v.cont}</td>
+            </tr>
+        `
+          )
+          .join("")}
+    </table>
+    `;
+}; //////////// showBoard //////////////
+
+// 만약 로컬쓰가 있으면 게시판 출력하기! 최초호출!
+const checkLocals = localStorage.getItem("my-board");
+if (checkLocals) {
+  showBoard(JSON.parse(checkLocals));
+} ///// if ///////////////
+// 만약 로컬쓰가 없으면 없음표시 게시판출력
+else {
+  boardBox.innerHTML = `
+    <table>
+        <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>내용</th>
+        </tr>
+        <tr>
+            <td colspan="3">데이터가 없습니다</td>
+        </tr>
+    </table>
+    `;
+}
